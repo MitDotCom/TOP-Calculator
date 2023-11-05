d28da1aa99399954fd63e593e3385cdd066a7647
Mitchell <Mitchellrcomrie@gmail.com>
2023-11-05 10:11:03 -0800

    Add DOM nodes and event listeners for calculator buttons in script

---
+++ script.js
@@ -0,0 +1,56 @@
+// CONSTANTS
+
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+const sevenBtn = document.querySelector('.seven');
+const eightBtn = document.querySelector('.eight');
+const nineBtn = document.querySelector('.nine');
+const divideBtn = document.querySelector('.divide');
+const fourBtn = document.querySelector('.four');
+const fiveBtn = document.querySelector('.five');
+const sixBtn = document.querySelector('.six');
+const multiplyBtn = document.querySelector('.multiply');
+const oneBtn = document.querySelector('.one');
+const twoBtn = document.querySelector('.two');
+const threeBtn = document.querySelector('.three');
+const minusBtn = document.querySelector('.minus');
+const decimalBtn = document.querySelector('.decimal');
+const zeroBtn = document.querySelector('.zero');
+const equalsBtn = document.querySelector('.equals');
+const plusBtn = document.querySelector('.plus');
+
+clearBtn.addEventListener('click', function(e) { });
+
+deleteBtn.addEventListener('click', function(e) { });
+
+sevenBtn.addEventListener('click', function(e) { });
+
+eightBtn.addEventListener('click', function(e) { });
+
+nineBtn.addEventListener('click', function(e) { });
+
+divideBtn.addEventListener('click', function(e) { });
+
+fourBtn.addEventListener('click', function(e) { });
+
+fiveBtn.addEventListener('click', function(e) { });
+
+sixBtn.addEventListener('click', function(e) { });
+
+multiplyBtn.addEventListener('click', function(e) { });
+
+oneBtn.addEventListener('click', function(e) { });
+
+twoBtn.addEventListener('click', function(e) { });
+
+threeBtn.addEventListener('click', function(e) { });
+
+minusBtn.addEventListener('click', function(e) { });
+
+decimalBtn.addEventListener('click', function(e) { });
+
+zeroBtn.addEventListener('click', function(e) { });
+
+equalsBtn.addEventListener('click', function(e) { });
+
+plusBtn.addEventListener('click', function(e) { });
